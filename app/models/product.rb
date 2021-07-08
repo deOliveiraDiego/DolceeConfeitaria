1d84d5cc364e6e8a3e9396f6f68c6e30ccd7fbda
deOliveiraDiego <diego.oliveira.silva.07@gmail.com>
2021-07-07 23:18:02 -0300

    Remove attr_accessible.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: false
 
 class Product < ApplicationRecord
-  attr_accessible :name, :description, :price, :dimensions
-
   validates :name, :description, :price, presence: true
 end
